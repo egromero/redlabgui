945af8c29fff0f4e2ed68701843c16a9863c0fa7
savelozo <savelozo@uc.cl>
2024-05-23 17:30:49 -0400

    Fix API Key

--- credentials.py
+++ credentials.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+import os 
 #persona_uc_credential = {'x-api-key': 'NytsPEeU5svGbZfdzk5azGt4JgrMr0YYe6Cqbbvc'}
 #tarjeta_uc_credential = {'x-api-key': 'T1UJYenP3Tb3CFj06B4bjYcjDMISO4j4EOMhsEK8'}
 persona_uc_credential = {'x-api-key': 'Rs05SNcp1SOBXYkw3KTyxUBP6b1iinWAq7IbiAE6'}
 tarjeta_uc_credential = {'x-api-key': 'Rs05SNcp1SOBXYkw3KTyxUBP6b1iinWAq7IbiAE6'}
 totem_credential = {'x-api-key': "vTXpCA1olTt0oQdYQDuFqwtt"}
-api_key_airtable = {'x-api-key': "patF2SVf4AuwV1aU6.6ed93cf3188c50268d9fa306fe4740227dff23c2835b1e2da1278141196742fd"}
+api_key_airtable = {'x-api-key': os.getenv('API_KEY')}
