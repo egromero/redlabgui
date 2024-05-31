77c8d61c38317d0ea4d73353a998694f9d63accd
savelozo <savelozo@uc.cl>
2024-05-31 11:51:19 -0400

    Add loggin to apiHandler rotated

--- apiHandler.py
+++ apiHandler.py
@@ -14,18 +14,18 @@ def get_data(uid):
     logging.info("Entrando a get - rfid: {}".format(uid))
     rotated = False
     url_tarjeta_uc = 'https://api.uc.cl/tarjetauc/v1/user/{0}?buscar=mifare'.format(uid)
-    logging.info("Haciendo request a API UC")
+    logging.info("Haciendo request a API UC: {0}".format(url_tarjeta_uc))
     uc_card = requests.get(url_tarjeta_uc, headers = tarjeta_uc_credential).json()
-    logging.info("UC card recibida: {}".format(uc_card))
     if uc_card['status']==300:
         rotated = True
         print("rotated")
         url_tarjeta_uc = 'https://api.uc.cl/tarjetauc/v1/user/{0}?buscar=mifare'.format(rotate(uid))
         new_uc_card = requests.get(url_tarjeta_uc, headers = tarjeta_uc_credential).json()
-        data_tarjeta = new_uc_card['tarjetauc']['data']        
-    else:
-        
+        data_tarjeta = new_uc_card['tarjetauc']['data']
+        logging.info("UC card recibida: {}".format(data_tarjeta))     
+    else:        
         data_tarjeta = uc_card['tarjetauc']['data']
+        logging.info("UC card recibida: {}".format(data_tarjeta))
         
     if isinstance(data_tarjeta, str):
         return data_tarjeta
@@ -52,9 +52,9 @@ def get_data(uid):
             'major': majors[0] }
     
 
-print(tarjeta_uc_credential)
-url_tarjeta_uc = 'https://api.uc.cl/tarjetauc/v1/user/{0}?buscar=mifare'.format('F996A0F8')
-print(url_tarjeta_uc)
-print("Haciendo request a API UC")
-uc_card = requests.get(url_tarjeta_uc, headers = tarjeta_uc_credential).json()
-print(uc_card)
+# print(tarjeta_uc_credential)
+# url_tarjeta_uc = 'https://api.uc.cl/tarjetauc/v1/user/A31802C7?buscar=mifare'
+# print(url_tarjeta_uc)
+# print("Haciendo request a API UC")
+# uc_card = requests.get(url_tarjeta_uc, headers = tarjeta_uc_credential).json()
+# print(uc_card)
